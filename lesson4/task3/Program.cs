52fb24645cfe628f0500237a1acfb3738add15b9
Виталий <mardonzz@yandex.ru>
2024-03-07 22:26:49 +0300

    lesson4 add task3

---
+++ lesson4/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿string CreatLatters(string s)
+
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = CreatLatters(str);
+Console.WriteLine(result);
